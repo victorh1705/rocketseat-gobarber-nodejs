201e0acf27d15ba4e0b56aba4bdfa285ae9935a9
Victor <victorh1705@gmail.com>
2020-05-20 11:35:19 -0300

    feature adicao do cors

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
+
 import routes from './routes';
 import AppError from './errors/AppError';
 
@@ -9,6 +11,7 @@ import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
